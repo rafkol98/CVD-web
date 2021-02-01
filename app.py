4a43570ba8f32232aebc2cab30665a8c9d57f233
rafaelkoll <rafcall98@hotmail.com>
2021-02-01 17:38:24 +0100

    Progress -> Bug with request on form

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
+model = pickle.load(open('cvd-model.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -10,6 +13,21 @@ def index():
 @app.route('/diagnose')
 def diagnose():
     return render_template('diagnose.html')
+
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+# Bug with request.
+    ints = [int(x) for x in request.form.values()]
+    final = [np.array(ints)]
+    prediction = model.predict(final)
+
+    if prediction==1:
+        return render_template('diagnose.html', pred='Cancer')
+    else:
+        return render_template('diagnose.html', pred='Healthy')
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
